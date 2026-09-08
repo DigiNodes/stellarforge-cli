64210b88eb26df69e4eb36cc2c67ea452a1f7bfc
Ahmed Mahmud <ahmedmahmud1994@hotmail.com>
2026-09-08 22:24:29 +0100

    fix: use explicit global console in Basic App

--- templates/basic-app/src/index.js
+++ templates/basic-app/src/index.js
@@ -2,6 +2,6 @@ import { getNetworkConfig } from './network.js';
 
 const network = getNetworkConfig();
 
-console.log(`{{projectName}} is configured for Stellar Testnet.`);
-console.log(`Horizon: ${network.horizonUrl}`);
-console.log(`RPC: ${network.rpcUrl}`);
+globalThis.console.log(`{{projectName}} is configured for Stellar Testnet.`);
+globalThis.console.log(`Horizon: ${network.horizonUrl}`);
+globalThis.console.log(`RPC: ${network.rpcUrl}`);
